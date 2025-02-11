1ffb39469b7709c23cacfa358f40444604cbace7
jesus <jesusleondiaz07@gmail.com>
2025-02-11 12:13:34 +0100

    Esqueleto Course junto SQL y la clase terminada

---
+++ gestionformacion/course-module/src/main/java/com/viewnext/course/business/services/CursoService.java
@@ -0,0 +1,5 @@
+package com.viewnext.course.business.services;
+
+public interface CursoService {
+
+}
---
+++ gestionformacion/course-module/src/main/java/com/viewnext/course/presentation/controller/CursoController.java
@@ -0,0 +1,5 @@
+package com.viewnext.course.presentation.controller;
+
+public class CursoController {
+
+}
