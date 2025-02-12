a8d1d7364acf776ac0e0af517a5b8f244162b8f2
Luis Arjona Aguilar <luisarjonaaguilar@gmail.com>
2025-02-12 10:37:30 +0100

    Añadir anotacion a CursoRepository

--- gestionformacion/course-module/src/main/java/com/viewnext/course/integration/repository/CursoRepository.java
+++ gestionformacion/course-module/src/main/java/com/viewnext/course/integration/repository/CursoRepository.java
@@ -1,9 +1,11 @@
 package com.viewnext.course.integration.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.viewnext.course.business.model.Course;
 
+@Repository
 public interface CursoRepository extends JpaRepository<Course, Long>{
 	 
 }
