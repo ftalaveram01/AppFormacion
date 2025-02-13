10665cbac73489360a30430fb9a8df6604cbcad3
Fernando <ftalaveram01@gmail.com>
2025-02-13 13:36:43 +0100

    Añadidas dependencias de core a course

--- gestionformacion/course-module/src/main/java/com/viewnext/course/business/model/Course.java
+++ gestionformacion/course-module/src/main/java/com/viewnext/course/business/model/Course.java
@@ -2,6 +2,9 @@ package com.viewnext.course.business.model;
 
 import java.util.Date;
 import java.util.List;
+
+import com.viewnext.core.business.model.Usuario;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
