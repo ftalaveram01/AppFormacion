4a72d6f04ab9df99545555724c10aaeb50fed511
Luis Arjona Aguilar <luisarjonaaguilar@gmail.com>
2025-02-17 10:47:15 +0100

    Añadir rol y eliminar relacion curso-usuario en el script

--- script_bbdd/gestionformacion.sql
+++ script_bbdd/gestionformacion.sql
@@ -5,7 +5,8 @@ USE gestionformacion;
 CREATE TABLE usuario (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(100) UNIQUE NOT NULL,
-	password VARCHAR(20) NOT NULL
+	password VARCHAR(20) NOT NULL,
+    rol VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE curso (
@@ -13,17 +14,7 @@ CREATE TABLE curso (
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	nombre VARCHAR(100) NOT NULL,
 	descripcion VARCHAR(500) ,
-	fecha_inicio DATETIME NOT NULL,
-	fecha_fin DATETIME NOT NULL
+	fecha_inicio TIMESTAMP NOT NULL,
+	fecha_fin TIMESTAMP NOT NULL
 
-);
-
-CREATE TABLE usuario_curso (
-
-	id_curso BIGINT NOT NULL,
-	id_usuario INT NOT NULL,
-    PRIMARY KEY (id_curso, id_usuario),
-    FOREIGN KEY (id_curso) REFERENCES curso(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
-    
 );
