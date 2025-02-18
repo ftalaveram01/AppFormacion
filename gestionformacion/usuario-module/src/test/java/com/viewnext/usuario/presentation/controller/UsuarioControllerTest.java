54d160e408f8522450156aaaa514bbabf89ee333
Luis Arjona Aguilar <luisarjonaaguilar@gmail.com>
2025-02-18 13:20:47 +0100

    Crear paquete de tests

---
+++ gestionformacion/usuario-module/src/test/java/com/viewnext/usuario/presentation/controller/UsuarioControllerTest.java
@@ -0,0 +1,8 @@
+package com.viewnext.usuario.presentation.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(UsuarioController.class)
+public class UsuarioControllerTest {
+
+}
