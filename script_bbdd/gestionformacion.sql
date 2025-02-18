9da2dda6eb5a7a15bb584efaad4d0c0cb1ceea86
Luis Arjona Aguilar <luisarjonaaguilar@gmail.com>
2025-02-18 09:57:50 +0100

    Crear tabla rol en la base de datos

--- script_bbdd/gestionformacion.sql
+++ script_bbdd/gestionformacion.sql
@@ -2,11 +2,22 @@ DROP DATABASE IF EXISTS gestionformacion;
 CREATE DATABASE gestionformacion CHARACTER SET utf8mb4;
 USE gestionformacion;
 
+CREATE TABLE rol (
+	
+    nombre_rol VARCHAR(200) NOT NULL UNIQUE,
+    descripcion VARCHAR(200),
+    PRIMARY KEY(nombre_rol)
+    
+);
+
 CREATE TABLE usuario (
+
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(100) UNIQUE NOT NULL,
 	password VARCHAR(20) NOT NULL,
-    rol VARCHAR(20) NOT NULL
+    rol VARCHAR(200) NOT NULL,
+    FOREIGN KEY(rol) REFERENCES rol(nombre_rol)
+    
 );
 
 CREATE TABLE curso (
@@ -18,3 +29,6 @@ CREATE TABLE curso (
 	fecha_fin TIMESTAMP NOT NULL
 
 );
+
+INSERT INTO rol (nombre_rol, descripcion) VALUES ('ADMIN', 'Usuario con todos los permisos'),
+('ALUMNO', 'Usuario normal');
