36dc2df7a03c80bfd8ef9ada4402c7ba477ddce2
Jesús <jesusleondiaz07@gmail.com>
2025-02-18 12:42:51 +0100

    Logs completados

--- gestionformacion/register-module/src/main/java/com/viewnext/register/RegisterApp.java
+++ gestionformacion/register-module/src/main/java/com/viewnext/register/RegisterApp.java
@@ -3,9 +3,15 @@ package com.viewnext.register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import com.viewnext.core.presentation.config.LogAspect;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.viewnext.core.business.model"})
+@EnableAspectJAutoProxy 
+@Import(LogAspect.class)
 public class RegisterApp {
 
 	public static void main(String[] args) {
