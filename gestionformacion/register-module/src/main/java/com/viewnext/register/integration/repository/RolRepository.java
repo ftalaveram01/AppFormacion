3ce486205602f1fbe1cb4890ce1e5c0f1046191c
Luis Arjona Aguilar <luisarjonaaguilar@gmail.com>
2025-02-18 13:32:25 +0100

    Actualizar registro para que pida el Usuario

---
+++ gestionformacion/register-module/src/main/java/com/viewnext/register/integration/repository/RolRepository.java
@@ -0,0 +1,11 @@
+package com.viewnext.register.integration.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.viewnext.core.business.model.Rol;
+import com.viewnext.core.business.model.RolEnum;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
+	Rol findByNombreRol(RolEnum nombreRol);
+}
