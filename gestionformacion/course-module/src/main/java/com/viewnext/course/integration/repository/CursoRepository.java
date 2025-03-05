9ad27c78cf4ccb603dd03e87a1aba9d24f7b9d32
Miguel Alonso <miguel.alonsor9@gmail.com>
2025-03-05 11:56:00 +0100

    sin cambios importantes

--- gestionformacion/course-module/src/main/java/com/viewnext/course/integration/repository/CursoRepository.java
+++ gestionformacion/course-module/src/main/java/com/viewnext/course/integration/repository/CursoRepository.java
@@ -1,14 +1,10 @@
 package com.viewnext.course.integration.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.viewnext.core.business.model.Course;
 
-import jakarta.transaction.Transactional;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Course, Long>{
