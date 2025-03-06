0af08273c33cff2c2314d09d606261c8d44c45f3
ADOLFO IGLESIAS GOMEZ <6003289@viewnext.com>
2025-03-06 12:23:07 +0100

    Arreglo formatear fechas

--- frontend/gestionUsuarios/src/app/Component/curso-list/curso-list.component.ts
+++ frontend/gestionUsuarios/src/app/Component/curso-list/curso-list.component.ts
@@ -120,7 +120,7 @@ export class CursoListComponent implements OnInit{
     localStorage.clear();
   }
 
-  formatearFecha(fecha: Date): string {
+  private formatearFecha(fecha: Date): string {
     const anio = fecha.getFullYear();
     const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
     const dia = fecha.getDate().toString().padStart(2, '0');
