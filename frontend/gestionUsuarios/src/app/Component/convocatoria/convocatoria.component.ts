d407f304017aa8e27598374303d0190275f7352f
ADOLFO IGLESIAS GOMEZ <6003289@viewnext.com>
2025-03-11 12:36:30 +0100

    integrado servicio y componente de convocatoria

--- frontend/gestionUsuarios/src/app/Component/convocatoria/convocatoria.component.ts
+++ frontend/gestionUsuarios/src/app/Component/convocatoria/convocatoria.component.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ConvocatoriaService } from '../../Services/convocatoria.service';
+
 
 @Component({
   selector: 'app-convocatoria',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './convocatoria.component.html',
   styleUrl: './convocatoria.component.css'
 })
 export class ConvocatoriaComponent {
+
+  convocatorias: any[] = [];
+  idAdmin!: number;
+  formulario!: FormGroup
+
+  constructor(private convocatoriaService: ConvocatoriaService, private router: Router) {
+  }
+
+  btnCreateConvocatoria(isUpdate: boolean, isCreate: boolean): void {
+    console.log(isCreate)
+    console.log(isUpdate)
+    this.router.navigate(['convocatoria/form'], {
+      queryParams: { isUpdate: isUpdate, isCreate: isCreate }
+    })
+  }
+
+  btnUpdateConvocatoria(isUpdate: boolean, isCreate: boolean, id: number): void {
+    console.log(isCreate)
+    console.log(isUpdate)
+    this.router.navigate(['Convocatoria/form'], {
+      queryParams: { isUpdate: isUpdate, isCreate: isCreate, id: id }
+    });
+  }
+
+  btnDeleteConvocatoria(id: number): void {
+    this.convocatoriaService.deleteConvocatoria(id).subscribe(() => {
+      this.convocatorias = this.convocatorias.filter(c => c.id !== id);
+    });
+  }
+
+  ngOnInit(): void {
+    this.convocatoriaService.getConvocatorias().subscribe((data) => {
+      this.convocatorias = data;
+    })
+    this.formulario = new FormGroup({
+      idCurso: new FormControl('')
+    })
+  }
+
+  borrarCache(): void {
+    localStorage.clear();
+  }
+
+  isAdmin(): boolean {
+    return this.idAdmin == 0;
+  }
 
 }
