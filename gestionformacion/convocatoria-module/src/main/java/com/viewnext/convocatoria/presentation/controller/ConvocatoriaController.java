a3fa2c68c603a5ab52e2807102cbcd9386e2c67c
LuisArjona <luisarjonaaguilar@gmail.com>
2025-03-12 10:23:50 +0100

    Update y Delete convocatoria en el controller

--- gestionformacion/convocatoria-module/src/main/java/com/viewnext/convocatoria/presentation/controller/ConvocatoriaController.java
+++ gestionformacion/convocatoria-module/src/main/java/com/viewnext/convocatoria/presentation/controller/ConvocatoriaController.java
@@ -1,6 +1,10 @@
 package com.viewnext.convocatoria.presentation.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.viewnext.convocatoria.business.services.ConvocatoriaServices;
 import com.viewnext.convocatoria.model.ConvocatoriaRequest;
+import com.viewnext.convocatoria.model.UpdateRequest;
 import com.viewnext.core.business.model.Convocatoria;
 
 @RestController
@@ -24,5 +29,18 @@ public class ConvocatoriaController {
 	public Convocatoria create(@RequestBody ConvocatoriaRequest request, @RequestParam Long idAdmin) {
 		return convocatoriaServices.create(idAdmin, request);
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable Long id, @RequestParam Long idAdmin, @RequestBody UpdateRequest request){
+		convocatoriaServices.update(id, idAdmin, request);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id, @RequestParam Long idAdmin){
+		convocatoriaServices.delete(id, idAdmin);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
