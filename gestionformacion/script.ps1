0016026f211fa57e56ede1b3738cba5d64c3949b
Jesús <jesusleondiaz07@gmail.com>
2025-03-21 13:04:35 +0100

    Arreglos de Docker completo

--- gestionformacion/script.ps1
+++ gestionformacion/script.ps1
@@ -13,27 +13,27 @@ mvn clean package -DskipTests
 
 Set-Location -Path $rutaConvocatoria
 docker build -t "convocatoria-app" .
-Write-Host "La imagen de convocatoria-app a sido creada"
+Write-Host "La imagen de convocatoria-app ha sido creada"
 
 Set-Location -Path $rutaCourse
 docker build -t "course-app" .
-Write-Host "La imagen de course-app a sido creada"
+Write-Host "La imagen de course-app ha sido creada"
 
 Set-Location -Path $rutaLogin
 docker build -t "login-app" .
-Write-Host "La imagen de login-app a sido creada"
+Write-Host "La imagen de login-app ha sido creada"
 
 Set-Location -Path $rutaRegister
 docker build -t "register-app" .
-Write-Host "La imagen de register-app a sido creada"
+Write-Host "La imagen de register-app ha sido creada"
 
 Set-Location -Path $rutaRol
 docker build -t "rol-app" .
-Write-Host "La imagen de rol-app a sido creada"
+Write-Host "La imagen de rol-app ha sido creada"
 
 Set-Location -Path $rutaUsuario
 docker build -t "usuario-app" .
-Write-Host "La imagen de usuario-app a sido creada"
+Write-Host "La imagen de usuario-app ha sido creada"
 
 
 Set-Location -Path $rutaProyecto
