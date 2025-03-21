106177453b6fbbf3fab3c3cd7e0813724319138f
LuisArjona <luisarjonaaguilar@gmail.com>
2025-03-21 09:30:56 +0100

    Security y cifrado de contraseñas funcionando en módulo de Registro

--- gestionformacion/register-module/src/main/java/com/viewnext/register/RegisterApp.java
+++ gestionformacion/register-module/src/main/java/com/viewnext/register/RegisterApp.java
@@ -3,6 +3,7 @@ package com.viewnext.register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,8 @@ import com.viewnext.core.presentation.config.LogAspect;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.viewnext.core.business.model"})
+@ComponentScan(basePackages = {"com.viewnext.core.presentation.config", "com.viewnext.register",
+"com.viewnext.core.security"})
 @EnableJpaRepositories(basePackages = {"com.viewnext.core.repositories"})
 @EnableAspectJAutoProxy 
 @Import(LogAspect.class)
