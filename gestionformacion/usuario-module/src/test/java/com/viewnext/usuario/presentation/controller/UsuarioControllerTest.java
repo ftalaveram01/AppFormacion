7bc0ef5bebb689abf82e40d19f80a4f3be0e9729
LuisArjona <luisarjonaaguilar@gmail.com>
2025-03-27 08:17:59 +0100

    Completar tests de UsuarioController

--- gestionformacion/usuario-module/src/test/java/com/viewnext/usuario/presentation/controller/UsuarioControllerTest.java
+++ gestionformacion/usuario-module/src/test/java/com/viewnext/usuario/presentation/controller/UsuarioControllerTest.java
@@ -7,43 +7,111 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.testcontainers.containers.MySQLContainer;
 
-import com.viewnext.core.business.model.Rol;
-import com.viewnext.core.business.model.Usuario;
-import com.viewnext.usuario.business.services.UsuarioServices;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.viewnext.core.business.model.Rol;
+import com.viewnext.core.business.model.RolAuthority;
+import com.viewnext.core.business.model.Usuario;
+import com.viewnext.core.repositories.UsuarioRepository;
+import com.viewnext.core.security.UsuarioDetails;
+import com.viewnext.core.security.UsuarioDetailsService;
+import com.viewnext.core.security.UtilsJWT;
+import com.viewnext.usuario.business.services.UsuarioServices;
 
-@WebMvcTest(UsuarioController.class)
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class UsuarioControllerTest {
+	
+	 @LocalServerPort
+	 private Integer port;
+	
+	@Autowired
+	private MockMvc mockMvc;
 
-    private MockMvc mockMvc;
-
-    @Mock
+    @MockitoBean
     private UsuarioServices usuarioServices;
 
-    @InjectMocks
     private UsuarioController usuarioController;
+    
+    @MockitoBean
+    private UtilsJWT utilsJWT;
+    
+    @MockitoBean
+    private UsuarioRepository usuarioRepository;
+    
+    @MockitoBean
+    private UsuarioDetailsService usuarioDetailsService;
+	
+    @Value("${gestionformacion.secreto.jwt}")
+    private String jwtSecret;
+    
+    @Value("${gestionformacion.tiempo.expiracion.jwt}")
+    private int jwtExpirationMs;
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(usuarioController).build();
+    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:9.2.0")
+    		.withDatabaseName("gestionformacion")
+            .withUsername("user")
+            .withPassword("password")
+            .withInitScript("schema.sql");;
+    
+    @BeforeAll
+    static void beforeAll() {
+    	mysqlContainer.start();
     }
 
+    @AfterAll
+    static void afterAll() {
+    	mysqlContainer.stop();
+    }
+    
+    @BeforeEach
+    void setUp() {
+      RestAssured.baseURI = "http://localhost:" + port;
+      usuarioController = new UsuarioController(usuarioServices);
+    }
+	
+	@DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+      registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+      registry.add("spring.datasource.username", mysqlContainer::getUsername);
+      registry.add("spring.datasource.password", mysqlContainer::getPassword);
+    }
+	
     @Test
     void testCreate() throws Exception {
+    	
+    	this.mockAdmin();
+		String jwtToken = "Bearer " + this.generateJwtAdmin();
+		 
         Rol rol= new Rol();
         rol.setId(1L);
 
@@ -55,25 +123,67 @@ class UsuarioControllerTest {
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/usuarios/crear")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(usuario)))
+                .content(objectMapper.writeValueAsString(usuario))
+                .header("Authorization", jwtToken))
                 .andExpect(status().isCreated())
                 .andReturn();
 
         verify(usuarioServices, times(1)).create(any(Usuario.class));
         assertEquals("http://localhost/usuarios/1", result.getResponse().getHeader("Location"));
     }
+    
+    @Test
+    void testCreateNoAdmin() throws Exception {
+    	
+    	this.mockAlumno();
+		String jwtToken = "Bearer " + this.generateJwtAlumno();
+		 
+        Rol rol= new Rol();
+        rol.setId(1L);
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("prueba@gmail.com");
+        usuario.setPassword("1234");
+        usuario.setRol(rol);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/usuarios/crear")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(usuario))
+                .header("Authorization", jwtToken))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
 
     @Test
     void testDelete() throws Exception {
+    	
+    	this.mockAdmin();
+		String jwtToken = "Bearer " + this.generateJwtAdmin();
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/usuarios/borrar/1"))
+        mockMvc.perform(MockMvcRequestBuilders.delete("/usuarios/borrar/1")
+        		.header("Authorization", jwtToken))
                 .andExpect(status().isNoContent());
 
         verify(usuarioServices, times(1)).delete(1L);
     }
+    
+    @Test
+    void testDeleteNoAdmin() throws Exception {
+    	
+    	this.mockAlumno();
+		String jwtToken = "Bearer " + this.generateJwtAlumno();
+		
+        mockMvc.perform(MockMvcRequestBuilders.delete("/usuarios/borrar/1")
+        		.header("Authorization", jwtToken))
+                .andExpect(status().isUnauthorized());
+    }
 
     @Test
     void testUpdate() throws Exception {
+    	
+    	this.mockAdmin();
+		String jwtToken = "Bearer " + this.generateJwtAdmin();
+    	
         Rol rol= new Rol();
         rol.setId(1L);
 
@@ -84,24 +194,76 @@ class UsuarioControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.put("/usuarios/actualizar")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(usuario)))
+                .content(objectMapper.writeValueAsString(usuario))
+                .header("Authorization", jwtToken))
                 .andExpect(status().isNoContent());
 
         verify(usuarioServices, times(1)).update(any(Usuario.class));
     }
-
+    
     @Test
-    void testGetAll() throws Exception {
-        when(usuarioServices.getAll()).thenReturn(java.util.Collections.emptyList());
+    void testUpdateNoAdmin() throws Exception {
+    	
+    	this.mockAlumno();
+		String jwtToken = "Bearer " + this.generateJwtAlumno();
+    	
+        Rol rol= new Rol();
+        rol.setId(1L);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/usuarios"))
-                .andExpect(status().isOk());
+        Usuario usuario = new Usuario();
+        usuario.setEmail("prueba@gmail.com");
+        usuario.setPassword("1234");
+        usuario.setRol(rol);
 
-        verify(usuarioServices, times(1)).getAll();
+        mockMvc.perform(MockMvcRequestBuilders.put("/usuarios/actualizar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(usuario))
+                .header("Authorization", jwtToken))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
+    void testGetAll() throws Exception {
+    	
+    	Rol rol= new Rol();
+        rol.setId(1L);
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("prueba@gmail.com");
+        usuario.setPassword("1234");
+        usuario.setRol(rol);
+    	
+        this.mockAdmin();
+		String jwtToken = "Bearer " + this.generateJwtAdmin();
+		
+        when(usuarioServices.getAll()).thenReturn(Arrays.asList(usuario));
+
+        MvcResult response = mockMvc.perform(MockMvcRequestBuilders.get("/usuarios")
+        		.header("Authorization", jwtToken))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        verify(usuarioServices, times(1)).getAll();
+        assertEquals(objectMapper.writeValueAsString(Arrays.asList(usuario)), response.getResponse().getContentAsString());
+    }
+    
+    @Test
+    void testGetAllNoAdmin() throws Exception {
+    	
+        this.mockAlumno();
+		String jwtToken = "Bearer " + this.generateJwtAlumno();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/usuarios")
+        		.header("Authorization", jwtToken))
+                .andExpect(status().isUnauthorized());
+    }
+    
+    @Test
     void testRead() throws Exception {
+    	
+        this.mockAdmin();
+		String jwtToken = "Bearer " + this.generateJwtAdmin();
+    	
         Rol rol= new Rol();
         rol.setId(1L);
 
@@ -111,11 +273,82 @@ class UsuarioControllerTest {
         usuario.setRol(rol);
         when(usuarioServices.read(any(Long.class))).thenReturn(usuario);
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/usuarios/1"))
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/usuarios/1")
+        		.header("Authorization", jwtToken))
                 .andExpect(status().isOk())
                 .andReturn();
 
         verify(usuarioServices, times(1)).read(1L);
         assertEquals(objectMapper.writeValueAsString(usuario), result.getResponse().getContentAsString());
+    }
+    
+
+    @Test
+    void testReadNoAdmin() throws Exception {
+    	
+        this.mockAlumno();
+		String jwtToken = "Bearer " + this.generateJwtAlumno();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/usuarios/1")
+        		.header("Authorization", jwtToken))
+                .andExpect(status().isUnauthorized());
+    }
+    
+    private String generateJwtAdmin() {
+        Rol admin = new Rol();
+        admin.setNombreRol("ADMIN");
+        return Jwts.builder()
+                .claim("email", "example100@example.com")
+                .claim("rol", Collections.singleton(new RolAuthority(admin)))
+                .setSubject("1")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(decodificarSecreto())
+                .compact();
+    }
+
+    private String generateJwtAlumno() {
+        Rol alumno = new Rol();
+        alumno.setNombreRol("ALUMNO");
+        return Jwts.builder()
+                .claim("email", "example100@example.com")
+                .claim("rol", Collections.singleton(new RolAuthority(alumno)))
+                .setSubject("1")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(decodificarSecreto())
+                .compact();
+    }
+    
+    private void mockAdmin() {
+    	Rol admin = new Rol();
+	     admin.setNombreRol("ADMIN");
+	     
+	     Usuario user = new Usuario(1L, "example100@example.com", "secreto", "password", admin, true);
+		 
+		 UsuarioDetails usuarioDetails = new UsuarioDetails(user);
+		 when(usuarioRepository.existsByEmail("example100@example.com")).thenReturn(true);
+	     when(usuarioRepository.findByEmail("example100@example.com")).thenReturn(user);
+	     when(usuarioDetailsService.loadUserByUsername("example100@example.com")).thenReturn(usuarioDetails);
+	     when(utilsJWT.validarJwt(any(String.class))).thenReturn(true);
+	     when(utilsJWT.getTokenUsername(any(String.class))).thenReturn("example100@example.com");
+    }
+    
+    private void mockAlumno() {
+    	Rol alumno = new Rol();
+	     alumno.setNombreRol("ALUMNO");
+	     
+	     Usuario user = new Usuario(1L, "example100@example.com", "secreto", "password", alumno, true);
+		 
+		 UsuarioDetails usuarioDetails = new UsuarioDetails(user);
+		 when(usuarioRepository.existsByEmail("example100@example.com")).thenReturn(true);
+	     when(usuarioRepository.findByEmail("example100@example.com")).thenReturn(user);
+	     when(usuarioDetailsService.loadUserByUsername("example100@example.com")).thenReturn(usuarioDetails);
+	     when(utilsJWT.validarJwt(any(String.class))).thenReturn(true);
+	     when(utilsJWT.getTokenUsername(any(String.class))).thenReturn("example100@example.com");
+    }
+    
+    private Key decodificarSecreto() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 }
