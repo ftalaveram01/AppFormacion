117409bd257e6d10fd5ae81bc519a3f88ccbf8c2
LuisArjona <luisarjonaaguilar@gmail.com>
2025-04-07 15:27:17 +0200

    Gateway funcionando correctamente (sin Docker)

--- api-gateway/src/main/java/com/viewnext/apigateway/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/viewnext/apigateway/api_gateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.viewnext.apigateway.api_gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
