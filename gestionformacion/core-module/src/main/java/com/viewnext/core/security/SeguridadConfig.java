a0349592befa3474b78e46198d8b1b76c8450a25
LuisArjona <luisarjonaaguilar@gmail.com>
2025-04-08 15:54:49 +0200

    Otra prueba mas de cors

--- gestionformacion/core-module/src/main/java/com/viewnext/core/security/SeguridadConfig.java
+++ gestionformacion/core-module/src/main/java/com/viewnext/core/security/SeguridadConfig.java
@@ -1,5 +1,7 @@
 package com.viewnext.core.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableMethodSecurity
 @Configuration
@@ -21,7 +26,7 @@ public class SeguridadConfig {
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-        	.cors(cors -> cors.disable())
+    		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/autentificacion/**").permitAll()
@@ -30,6 +35,16 @@ public class SeguridadConfig {
             .addFilterBefore(filtroAutentificacionOTP(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.applyPermitDefaultValues();
+	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**",configuration);
+	    return source;
 	}
     
     @Bean
