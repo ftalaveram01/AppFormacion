885a895e8116c1ce6fb32b351bf400dec3f220be
LuisArjona <luisarjonaaguilar@gmail.com>
2025-04-09 13:08:31 +0200

    Mejorar login y registro con más validaciones

--- frontend/gestionUsuarios/src/app/Component/register/register.component.ts
+++ frontend/gestionUsuarios/src/app/Component/register/register.component.ts
@@ -5,11 +5,15 @@ import { AuthService } from '../../Services/auth.service';
 import { Router } from '@angular/router';
 import { UserService } from '../../Services/user.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, InputTextModule, PasswordModule, KeyFilterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -21,17 +25,16 @@ export class RegisterComponent implements OnInit {
   rol: any = []
   urlQR!: SafeResourceUrl
   codigoAuthQR!: string
+  bloquearEspacios: RegExp = /^[^\s]*$/;
 
   constructor(private authService: AuthService, private fb: FormBuilder, private router: Router, private userService: UserService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      email: ['', Validators.required, this.emailValidator],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    }, {
-      validators: this.passwordMatchValidator
-    })
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, this.validarPass.bind(this)]],
+      confirmPassword: ["", [Validators.required]]
+    });
   }
 
   onSubmit(): void {
@@ -39,6 +42,20 @@ export class RegisterComponent implements OnInit {
       this.succes = true
       this.validUser();
     }
+  }
+
+  validarPass(control: AbstractControl): { [key: string]: boolean } | null {
+    const value = control.value;
+    if (!value) return null;
+  
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumber = /[0-9]/.test(value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    const hasEightChars = value.length >= 8;
+    const isValid = hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && hasEightChars;
+  
+    return !isValid ? { passwordComplexity: true } : null;
   }
 
   async emailValidator(control: any) {
@@ -90,5 +107,30 @@ export class RegisterComponent implements OnInit {
 
   get confirmPassword() {
     return this.form.get('confirmPassword');
+  }
+
+  tieneMayus(): boolean {
+    const value = this.form.get('password')?.value || '';
+    return /[A-Z]/.test(value);
+  }
+  
+  tieneMinus(): boolean {
+    const value = this.form.get('password')?.value || '';
+    return /[a-z]/.test(value);
+  }
+  
+  tieneNumero(): boolean {
+    const value = this.form.get('password')?.value || '';
+    return /[0-9]/.test(value);
+  }
+  
+  tieneCaracterEspecial(): boolean {
+    const value = this.form.get('password')?.value || '';
+    return /[!@#$%^&*(),.?":{}|<>]/.test(value);
+  }
+  
+  tiene8Caracteres(): boolean {
+    const value = this.form.get('password')?.value || '';
+    return value.length >= 8;
   }
 }
